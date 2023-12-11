885c189510d67f90602ac918fdb07d8360f310f2
angelo friello <angelofriello01@gmail.com>
2023-12-11 16:40:55 +0100

    add comics

---
+++ resources/views/home/home-page.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    
+  <div class="container">
+    <div class="row">
+      <div class="grid">
+        <div class="card">
+
+        </div>
+      </div>
+    </div>
+  </div>
+    
+    
+@endsection
